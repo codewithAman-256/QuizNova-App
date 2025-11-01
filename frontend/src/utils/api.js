08e6674cfa016fbc464ed947066139b96aff6a04
AmanKhanBaloch <balochaman256@gmail.com>
2025-11-01 11:01:54 +0530

    change url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL /*|| "http://localhost:5000/api"*/,
+  baseURL: "https://quiznova-app.onrender.com/api "
+  //*|| "http://localhost:5000/api"*/,
 });
 
 // ✅ Get all quizzes
