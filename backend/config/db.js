19207d5826503e3ec68c1ff0ec6060290e9c704e
AmanKhanBaloch <balochaman256@gmail.com>
2025-11-07 18:50:50 +0530

    update difficulty in backend quizschema

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,22 +1,13 @@
 import mongoose from "mongoose";
-import Quiz from "../models/Quiz.js";
 
 const connectDB = async () => {
-    try{
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`🟢 MongoDB Connected ${conn.connection.host}`);
-
-        const result = await Quiz.updateMany({}, [
-        { $set: { difficulty: { $toLower: "$difficulty" } } },
-        ]);
-
-        console.log(`✅ Updated ${result.modifiedCount} quizzes to lowercase difficulties.`);
-    process.exit();
-    } catch(error){
-        console.error(`❌ Error : ${error.message}`);
-        process.exit(1);
-    }
-}
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`🟢 MongoDB Connected ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`❌ Error : ${error.message}`);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
-
